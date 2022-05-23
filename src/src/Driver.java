d2ee3adcde628ba1639e023e6108fded5d68d332
amoots <amoots@emich.edu>
2022-05-23 14:54:31 -0400

    fix Eclipse project structure so it compiles and runs

--- src/src/Driver.java
+++ src/src/Driver.java
@@ -3,7 +3,7 @@ package src;
 public class Driver {
 
 	public static void main(String[] args) {
-		//system.out.println("Our team is now starting the project");
+		System.out.println("Our team is now starting the project");
 		Glad.glad();
 	}
 
--- src/src/Glad.java
+++ src/src/Glad.java
@@ -1,3 +1,5 @@
+package src;
+
 
 public class Glad {
 	public static void glad() {

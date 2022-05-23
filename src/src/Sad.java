14571fa1eb7c65fdab716d10f6bf4d16680668c6
Alex <apilon1@emich.edu>
2022-05-23 15:22:02 -0400

    Added really sad feature

--- src/src/Sad.java
+++ src/src/Sad.java
@@ -4,7 +4,10 @@ public class Sad {
 
 	public static void sad() {
 		System.out.println("We are not sad");
-
+		
 	}
-
+	
+	public static void reallySad() {
+		System.out.println("We are really not glad");
+	}
 }

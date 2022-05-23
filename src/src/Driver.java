a515e8e509d620cc67fceb48357dbd2788c33b6c
Alex <apilon1@emich.edu>
2022-05-23 15:15:58 -0400

    Added Sad.java and fixed issues with Eclipse on Alex's end

--- src/src/Driver.java
+++ src/src/Driver.java
@@ -5,6 +5,7 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("Our team is now starting the project");
 		Glad.glad();
+		Sad.sad();
 	}
 
 }
---
+++ src/src/Sad.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Sad {
+
+	public static void sad() {
+		System.out.println("We are not sad");
+
+	}
+
+}
